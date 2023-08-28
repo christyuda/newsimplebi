f5f32f7fb8d099c58ee6d4365530e9e4df070ce0
christyuda <chrisyuda@ulbi.ac.id>
2023-08-28 15:35:18 +0700

    test api

--- js/cihuy.js
+++ js/cihuy.js
@@ -1,5 +1,25 @@
-import { CihuyRootLink } from "https://c-craftjs.github.io/link/link.js";
-import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
-const linknya = CihuyId("link");
-linknya.appendChild(CihuyRootLink);
-CihuyRootLink("./dashboard");
+function apiGetRequest(url, callback) {
+  const getapi = new XMLHttpRequest();
+  getapi.open("GET", url, true);
+  getapi.onreadystatechange = () => {
+    if (getapi.readyState === 4) {
+      if (getapi.status === 200) {
+        const response = JSON.parse(getapi.responseText);
+        callback(null, response);
+      } else {
+        callback(new Error("Request failed"), null);
+      }
+    }
+  };
+  getapi.send();
+}
+
+const apiUrl = "https://simbe-dev.ulbi.ac.id/api/v1/admins/"; // Ganti dengan URL API yang sesuai
+apiGetRequest(apiUrl, (error, response) => {
+  if (error) {
+    console.error("Error:", error);
+  } else {
+    console.log("Response:", response);
+    // Lakukan manipulasi data atau tindakan lain dengan response dari API
+  }
+});
